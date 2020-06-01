ea64e203c825a13ed40e6e509793231aae97e312
Alexander Pavlenko <pava@bk.ru>
2020-06-01 20:47:16 +0700

    Previous Classes Added

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,78 @@
+package ru.netology.domain;
+
+public class Radio {
+    private int currentStation;
+    private int currentVolume;
+    private final int minStation = 0;
+    private final int maxStation = 9;
+    private final int minVolume = 0;
+    private final int maxVolume = 10;
+
+    public int getMinStation() {
+        return minStation;
+    }
+
+    public int getMaxStation() {
+        return maxStation;
+    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation > maxStation) {
+            return;
+        }
+        if (currentStation < minStation) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume > maxVolume) {
+            return;
+        }
+        if (currentVolume < minVolume) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public void increaseCurrentVolume() {
+        if (currentVolume < maxVolume) currentVolume++;
+    }
+
+    public void decreaseCurrentVolume() {
+        if (currentVolume > minVolume) currentVolume--;
+    }
+
+    public void nextStation() {
+        if (currentStation == maxStation) {
+            currentStation = minStation;
+            return;
+        }
+        currentStation++;
+    }
+
+    public void prevStation() {
+        if (currentStation == minStation) {
+            currentStation = maxStation;
+            return;
+        }
+        currentStation--;
+    }
+}
---
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -0,0 +1,111 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RadioTest {
+    @Test
+    void shouldInvalidMaxSetCurrentVolume() {
+        Radio radio = new Radio();
+        // Запомнить текущую громкость
+        int expected = radio.getCurrentVolume();
+        // Установить значение громкости больше максимальной
+        radio.setCurrentVolume(11);
+        // Проверить, что текущая громкость не изменилась
+        assertEquals(expected, radio.getCurrentVolume());
+    }
+
+    @Test
+    void shouldInvalidMinSetCurrentVolume() {
+        Radio radio = new Radio();
+        // Запомнить текущую громкость
+        int expected = radio.getCurrentVolume();
+        // Установить значение громкости больше максимальной
+        radio.setCurrentVolume(-1);
+        // Проверить, что текущая громкость не изменилась
+        assertEquals(expected, radio.getCurrentVolume());
+    }
+
+    @Test
+    void shouldIncreaseCurrentVolume() {
+        Radio radio = new Radio();
+        // Установить текущую громкость
+        radio.setCurrentVolume(9);
+        // Нажимать кнопку увеличения громкости 2 раза
+        radio.increaseCurrentVolume();
+        radio.increaseCurrentVolume();
+        // Проверить, что громкость равна максимально возможной
+        assertEquals(radio.getMaxVolume(), radio.getCurrentVolume());
+    }
+
+    @Test
+    void shouldDecreaseCurrentVolume() {
+        Radio radio = new Radio();
+        // Установить текущую громкость
+        radio.setCurrentVolume(1);
+        // Нажимать кнопку уменьшения громкости 2 раза
+        radio.decreaseCurrentVolume();
+        radio.decreaseCurrentVolume();
+        // Проверить, что громкость равна минимально возможной
+        assertEquals(radio.getMinVolume(), radio.getCurrentVolume());
+    }
+
+    @Test
+    void shouldSetCurrentStation() {
+        Radio radio = new Radio();
+        int expected = 5;
+        // Установить станцию 5
+        radio.setCurrentStation(expected);
+        // Проверить, что станция 5 теперь текущая
+        assertEquals(expected, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldInvalidMinSetCurrentStation() {
+        Radio radio = new Radio();
+        int expected = radio.getCurrentStation();
+        // Установить станцию меньше минимальной
+        radio.setCurrentStation(-1);
+        // Проверить, что текущая станция не изменилась
+        assertEquals(expected, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldInvalidMaxSetCurrentStation() {
+        Radio radio = new Radio();
+        int expected = radio.getCurrentStation();
+        // Установить станцию больше максимальной
+        radio.setCurrentStation(10);
+        // Проверить, что текущая станция не изменилась
+        assertEquals(expected, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldNextRadioStation() {
+        Radio radio = new Radio();
+        // Установить станцию 8
+        radio.setCurrentStation(8);
+        // Нажать 2 раза next
+        radio.nextStation();
+        radio.nextStation();
+        // Ожидаемый результат
+        int expected = 0;
+        // Проверить, что станция 0 теперь текущая
+        assertEquals(expected, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldPrevRadioStation() {
+        Radio radio = new Radio();
+        // Установить станцию 1
+        radio.setCurrentStation(1);
+        // Нажать 2 раза prev
+        radio.prevStation();
+        radio.prevStation();
+        // Ожидаемый результат
+        int expected = 9;
+        // Проверить, что станция 9 теперь текущая
+        assertEquals(expected, radio.getCurrentStation());
+    }
+}
